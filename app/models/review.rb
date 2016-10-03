8fff9029e58f29219b601ce4657f218f76fe9344
Marcus Richard <msrichard18@gmail.com>
2016-10-03 17:08:29 -0400

    updated unit tests, review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
   validates :rating, length: { in: 0..5 }, presence: true
-  validates :rating, format: { with: /^[+]?[1-9]\d{0,2}(\.\d)?$/ }
+  validates :rating, format: { with: /[+]?[0-5]{1}(\.\d)/ }
   validates :safety_rating, length: { in: 0..5 }, numericality: { only_integer: true }, presence: true
   validates :schools_rating, length: { in: 0..5 }, numericality: { only_integer: true }, presence: true
   validates :food_entertainment, length: { in: 0..5 }, numericality: { only_integer: true }, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Neighborhood do
+RSpec.describe User do
   let(:user){
     User.create(user_name: "Jeff", profile_pic_url: "http://bit.ly/2cYj2Ex")
   }
@@ -18,18 +18,11 @@ RSpec.describe Neighborhood do
     )
   }
 
-  let(:microhood2) {
-    Microhood.create(
-      street: "Main Lane",
-      zip: "02551",
-      neighborhood_id: neighborhood.id,
-      user_id: user.id
-    )
-  }
-
   it "has the expected properties" do
-    expect(neighborhood.name).to eq("Dorchester")
-    expect(neighborhood.microhoods).to eq([microhood, microhood2])
+    expect(microhood.user).to eq(user)
+    expect(microhood.neighborhood).to eq(neighborhood)
+    expect(microhood.street).to eq("Cherry Lane")
+    expect(microhood.zip).to eq("02451")
   end
 
 end
