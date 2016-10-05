e0b5fc364bf6d59ce4fbbce3ed75f31a2533478a
Frank <fjlanasa@gmail.com>
2016-10-05 10:34:03 -0400

    hound errors

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -3,44 +3,44 @@ require 'rails_helper'
 feature 'user sign in' do
   scenario 'user gets to login page from home page' do
     visit '/'
-    expect(page).to have_content "Reviews"
-    click_link "Login"
+    expect(page).to have_content 'Reviews'
+    click_link 'Login'
 
-    expect(page).to have_content "Log in"
-    expect(page).to have_content "Email"
-    expect(page).to have_content "Password"
+    expect(page).to have_content 'Log in'
+    expect(page).to have_content 'Email'
+    expect(page).to have_content 'Password'
   end
   scenario 'user successfully signs in' do
     visit '/users/sign_in'
 
-    expect(page).to have_content "Log in"
-    expect(page).to have_content "Email"
-    expect(page).to have_content "Password"
+    expect(page).to have_content 'Log in'
+    expect(page).to have_content 'Email'
+    expect(page).to have_content 'Password'
 
     user= FactoryGirl.create(:user)
 
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
 
-    click_button "Log in"
-    expect(page).to have_content "Signed in successfully"
+    click_button 'Log in'
+    expect(page).to have_content 'Signed in successfully'
   end
 
   scenario 'user attempts to sign in with blank form' do
     visit '/users/sign_in'
 
-    click_button "Log in"
-    expect(page).to have_content "Invalid Email or password"
+    click_button 'Log in'
+    expect(page).to have_content 'Invalid Email or password'
   end
 
   scenario 'user attempts to sign in without creating an account' do
     visit 'users/sign_in'
 
-    fill_in "Email", with: "exampleemail@gmail.com"
-    fill_in "Password", with: "mysupersecurepassword"
+    fill_in 'Email', with: 'exampleemail@gmail.com'
+    fill_in 'Password', with: 'mysupersecurepassword'
 
-    click_button "Log in"
-    expect(page).to have_content "Invalid Email or password"
+    click_button 'Log in'
+    expect(page).to have_content 'Invalid Email or password'
   end
 
   scenario 'user sign out' do
@@ -48,14 +48,14 @@ feature 'user sign in' do
     login_as(user, :scope => :user)
 
     visit '/'
-    expect(page).to have_content "Reviews"
-    expect(page).to have_content "Edit registration"
-    expect(page).to have_content "Logout"
+    expect(page).to have_content 'Reviews'
+    expect(page).to have_content 'Edit registration'
+    expect(page).to have_content 'Logout'
 
-    click_link "Logout"
-    expect(page).to have_content "Signed out successfully"
-    expect(page).to have_content "Reviews"
-    expect(page).to have_link "Register"
-    expect(page).to have_link "Login"
+    click_link 'Logout'
+    expect(page).to have_content 'Signed out successfully'
+    expect(page).to have_content 'Reviews'
+    expect(page).to have_link 'Register'
+    expect(page).to have_link 'Login'
   end
 end
