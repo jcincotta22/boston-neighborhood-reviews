3a09ecacb64ea9eaf8b5e3379b0e54f4383a2c8f
Frank <fjlanasa@gmail.com>
2016-10-05 10:08:20 -0400

    hound errors

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -2,8 +2,8 @@ require 'factory_girl'
 
 FactoryGirl.define do
   factory :user do
-    first_name "Frank"
-    last_name "LaNasa"
+    first_name 'Frank'
+    last_name 'LaNasa'
     sequence(:email) { |n| "user#{n}@gmail.com"}
     sequence(:password) { |n| "password#{n}!"}
   end
