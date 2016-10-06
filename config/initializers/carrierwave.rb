e0c01141c4f26dc568ade36afeececb0d4f36e98
jcincotta22 <jcincotta22@gmail.com>
2016-10-06 10:53:57 -0400

    silence the hound

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
       aws_access_key_id: ENV['AWS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
-    config.fog_directory  = ENV['BUCKET']
+    config.fog_directory = ENV['BUCKET']
     # config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
   end
 end
