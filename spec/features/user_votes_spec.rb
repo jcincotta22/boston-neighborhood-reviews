de18798677ad227a76313e89b43aef99f23ba3a5
Frank <fjlanasa@gmail.com>
2016-10-11 13:21:35 -0400

    vote spec 2

--- spec/features/user_votes_spec.rb
+++ spec/features/user_votes_spec.rb
@@ -20,4 +20,15 @@ feature 'user votes', js:true do
     click_button 'Upvote'
     expect(page).to have_content 'Vote count: 0'
   end
+
+  scenario 'user upvotes then switches to downvote' do
+    user = FactoryGirl.create(:user)
+    login_as(user, scope: :user)
+    microhood = FactoryGirl.create(:microhood)
+    review = FactoryGirl.create(:review, microhood: microhood)
+    visit microhood_path(microhood)
+    click_button 'Upvote'
+    click_button 'Downvote'
+    expect(page).to have_content 'Vote count: -1'
+  end
 end
