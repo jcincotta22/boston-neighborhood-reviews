0f505308360353391b8aa938d97e7131b0643975
Frank <fjlanasa@gmail.com>
2016-10-11 13:20:11 -0400

    vote spec

--- spec/features/user_votes_spec.rb
+++ spec/features/user_votes_spec.rb
@@ -10,5 +10,14 @@ feature 'user votes', js:true do
     click_button 'Upvote'
     expect(page).to have_content 'Vote count: 1'
   end
-
+  scenario 'usr upvotes twice which removes vote' do
+    user = FactoryGirl.create(:user)
+    login_as(user, scope: :user)
+    microhood = FactoryGirl.create(:microhood)
+    review = FactoryGirl.create(:review, microhood: microhood)
+    visit microhood_path(microhood)
+    click_button 'Upvote'
+    click_button 'Upvote'
+    expect(page).to have_content 'Vote count: 0'
+  end
 end
