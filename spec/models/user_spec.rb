e38e149b1e12f508c267972768b7bee69a7a1083
jcincotta22 <jcincotta22@gmail.com>
2016-10-12 18:18:51 -0400

    hound

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Review do
-  let(:user){
+  let(:user) do
     User.create(first_name: 'Jeff', last_name: 'Cinc', email: 'blah@gmail.com', password: '12345abc!A')
-  }
+  end
   it 'has the expected properties' do
     expect(user.first_name).to eq('Jeff')
     expect(user.last_name).to eq('Cinc')
