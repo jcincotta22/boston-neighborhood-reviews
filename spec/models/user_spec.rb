01b4001465e083f7772c08c82c2c591519243068
jcincotta22 <jcincotta22@gmail.com>
2016-10-12 18:17:02 -0400

    unit test for users

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Review do
+  let(:user){
+    User.create(first_name: 'Jeff', last_name: 'Cinc', email: 'blah@gmail.com', password: '12345abc!A')
+  }
+  it 'has the expected properties' do
+    expect(user.first_name).to eq('Jeff')
+    expect(user.last_name).to eq('Cinc')
+    expect(user.email).to eq('blah@gmail.com')
+    expect(user.password).to eq('12345abc!A')
+  end
+end
