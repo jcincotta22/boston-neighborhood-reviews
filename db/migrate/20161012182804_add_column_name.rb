f97537ccb3422ffa88b33abc146ab2f5b56cab89
Frank <fjlanasa@gmail.com>
2016-10-12 16:22:20 -0400

    merge conflict

--- db/migrate/20161012182804_add_column_name.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnName < ActiveRecord::Migration[5.0]
-  def up
-    add_column :microhoods, :name, :string, null: false
-  end
-
-  def down
-    remove_column :microhoods, :name
-  end
-end
