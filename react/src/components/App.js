0304071c28b5d598bf1a6f35ab966082d77d7f34
Frank <fjlanasa@gmail.com>
2016-10-12 14:01:05 -0400

    enabled microhood index page to update every 5 seconds

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -7,23 +7,35 @@ class App extends Component {
     this.state = {
       microhoods: []
     }
+
+    this.getIndex = this.getIndex.bind(this);
+    this.myInterval;
   }
 
-  componentDidMount() {
+  getIndex() {
     $.ajax({
       url: 'api/microhoods',
       contentType: 'application/json'
     })
     .done(data => {
-      this.setState({ microhoods: data.microhoods })
+      this.setState({ microhoods: data.microhoods });
+      console.log('hey');
     })
+  }
+
+  componentDidMount() {
+    this.getIndex();
+    this.myInterval = setInterval(this.getIndex, 5000);
   }
 
   render(){
 
     return(
-      <div>
+      <div className='callout'>
         <MicrohoodsList microhoods={this.state.microhoods} />
+        <div className='create-microhood-review-link'>
+          <a href='/microhoods/new'>Create and Review a New Microhood</a>
+        </div>
       </div>
     )
   }
