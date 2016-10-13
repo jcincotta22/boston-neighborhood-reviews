51b635f9ba3f65a710e46f8fc1cd3982ab4fb04f
Frank <fjlanasa@gmail.com>
2016-10-13 09:53:04 -0400

    changed relevant tests to account for change in index page

--- spec/features/user_adds_review_to_micro_hood_spec.rb
+++ spec/features/user_adds_review_to_micro_hood_spec.rb
@@ -6,7 +6,7 @@ feature 'user adds review to microhood', js: true do
     user = FactoryGirl.create(:user)
     visit root_path
     login_as(user, scope: :user)
-    click_link("#{microhoods.last.street}, #{microhoods.last.zip}")
+    click_link("#{microhoods.last.name}")
     fill_in('review_title', with: 'Ratings for Main St')
     fill_in('review_safety_rating', with: 4)
     fill_in('review_schools_rating', with: 4)
