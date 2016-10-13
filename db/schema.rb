88ada13be02c9e639f017ba51f4ac8e84231a57b
Frank <fjlanasa@gmail.com>
2016-10-13 09:30:27 -0400

    updated index and show page for microhoods to use name and details

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007134903) do
+ActiveRecord::Schema.define(version: 20161013131731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
