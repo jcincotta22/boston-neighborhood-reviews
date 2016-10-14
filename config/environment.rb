63e1b18f7f0f998788bf0c6bb0c59f086221d97b
jcincotta22 <jcincotta22@gmail.com>
2016-10-14 16:04:38 -0400

    minor change with sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-
-# ActionMailer::Base.smtp_settings = {
-#   user_name: 'app57789087@heroku.com',
-#   password: 'e2wremqi0005',
-#   domain: 'https://boston-neighborhoods-reviews.herokuapp.com/',
-#   address: 'smtp.sendgrid.net',
-#   port: 587,
-#   authentication: :plain,
-#   enable_starttls_auto: true
-# }
